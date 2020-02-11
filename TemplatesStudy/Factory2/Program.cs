483c9be374b17d67209c70f12ed80b8245e778e6
aleshin-roman-nk <aleshin.roman.nk@gmail.com>
2020-02-11 18:10:34 +0700

    test

---
+++ TemplatesStudy/Factory2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Factory2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+
+	public abstract class PizzaStore
+	{
+		public Pizza orderPizza(string type)
+		{
+			Pizza pizza = createPizza(type);
+
+			pizza.Prepare();
+			pizza.Bake();
+			pizza.Cut();
+			pizza.Box();
+
+			return pizza;
+		}
+
+		public abstract Pizza createPizza(string type);
+	}
+
+
+	public class PeppironiPizza : Pizza
+	{
+	}
+
+	public class GreekPizza : Pizza
+	{
+	}
+
+	public class CheesePizza : Pizza
+	{
+	}
+
+	public class Pizza
+	{
+		public void Bake()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Box()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Cut()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Prepare()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ TemplatesStudy/TemplatesStudy/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TemplatesStudy
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+
+
+			Console.ReadLine();
+		}
+	}
+
+	internal class PizzaStore
+	{
+		SimplePizzaFactory factory;
+
+		public PizzaStore(SimplePizzaFactory f)
+		{
+			this.factory = f;
+		}
+
+		public Pizza orderPizza(string type)
+		{
+			Pizza pizza = factory.create(type);
+
+			pizza.Prepare();
+			pizza.Bake();
+			pizza.Cut();
+			pizza.Box();
+
+			return pizza;
+		}
+	}
+
+	internal class SimplePizzaFactory
+	{
+		public Pizza create(string type)
+		{
+			Pizza pizza = null;
+
+			if (type.Equals("cheese"))
+				pizza = new CheesePizza();
+			else if (type.Equals("greek"))
+				pizza = new GreekPizza();
+			else if (type.Equals("peppironi"))
+				pizza = new PeppironiPizza();
+
+			return pizza;
+		}
+	}
+
+	internal class PeppironiPizza : Pizza
+	{
+	}
+
+	internal class GreekPizza : Pizza
+	{
+	}
+
+	internal class CheesePizza : Pizza
+	{
+	}
+
+	internal class Pizza
+	{
+		internal void Bake()
+		{
+			throw new NotImplementedException();
+		}
+
+		internal void Box()
+		{
+			throw new NotImplementedException();
+		}
+
+		internal void Cut()
+		{
+			throw new NotImplementedException();
+		}
+
+		internal void Prepare()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
